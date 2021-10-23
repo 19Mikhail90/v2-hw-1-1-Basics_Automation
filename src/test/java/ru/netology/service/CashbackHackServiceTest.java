fa390e05315db71c5f537464df75755f66a18a08
Mikhail Gozhenko <sacred9900@gmail.com>
2021-10-23 14:38:15 +0500

    change test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,10 +8,10 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldMissingAmount() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 800;
+        int amount = 1000;
 
         int actualResult = service.remain(amount);
-        int expectedResult = 200;
+        int expectedResult = 0;
 
         Assert.assertEquals(actualResult, expectedResult);
     }
